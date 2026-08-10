7fb8e5b40831e349d7f43e69e9f6db6a5a2540cd
keshavshre <147750320+keshavshre@users.noreply.github.com>
2026-08-10 18:12:17 +0530

    Time: 260 ms (57.94%) | Memory: 110.9 MB (12.96%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,23 +1,23 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int n=arr.length;
         int mod=(int)1e9+7;
+        int n=arr.length;
+        int []pse=new int[n];
+        int []nse=new int[n];
         Stack<Integer>s=new Stack<>();
-        int[]pse=new int[n];
-        int[]nse=new int[n];
-        for(int i=0;i<n;i++){
-            while(!s.isEmpty()&&arr[s.peek()]>arr[i]){
-                s.pop();
-            }
-            pse[i]=s.isEmpty()?-1:s.peek();
-            s.push(i);
-        }
-        s.clear();
         for(int i=n-1;i>=0;i--){
-            while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
+            while(!s.isEmpty() && arr[s.peek()]>arr[i]){
                 s.pop();
             }
             nse[i]=s.isEmpty()?n:s.peek();
+            s.push(i);
+        }
+        s.clear();
+        for(int i=0;i<n;i++){
+            while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            pse[i]=s.isEmpty()?-1:s.peek();
             s.push(i);
         }
         long sum=0;
